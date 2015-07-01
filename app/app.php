baf2a2df866b3228093da9732e68aacab3c0e96d
Jonatan Frank <jonatan@42mate.com>
2015-07-01 20:39:19 -0300

    db configuration

--- app/app.php
+++ app/app.php
@@ -1,11 +1,16 @@
 <?php
-
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 $app = new Silex\Application();
 
 include_once __DIR__ . '/../config/config.php';
 
-$app->get('/hello', function () {
-    return 'Hello!';
+$app->get('/franchises', function (Request $request) use ($app) {
+    $sql = "SELECT * FROM ODS_MDM_FRANCHISE_V";
+    $result = $app['db']->fetchAssoc($sql);
+    die(var_dump($result));
+    return new JsonResponse($result);
+
 });
 
 $app->run();
--- config/config.php
+++ config/config.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('max_execution_time', 300);
+$app['debug'] = true;
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
 	'db.options' => array(
 		'driver'   => 'oci8',
