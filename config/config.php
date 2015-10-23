623e59b48aea8d32a1746db0a33a50cae2395f8a
Jonatan Frank <jonafrank@gmail.com>
2015-10-22 21:04:03 -0300

    remove database info from config

--- config/config.php
+++ config/config.php
@@ -6,10 +6,10 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
 	'db.options' => array(
 		'driver'   => 'oci8',
 		'host'	   => null,
-		'dbname'   => '(DESCRIPTION=(LOAD_BALANCE=yes)(ADDRESS=(PROTOCOL=TCP)(HOST=odspddb.scan.rws.ad.ea.com)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=ODSPD)(failover_mode=(type=select)(method=basic))))',
+		'dbname'   => '', //Dbname Or TNS entry
 		'service'  => true,
-		'user'     => 'MDML_READ',
-		'password' => 'newmdmlread#2welcome',
+		'user'     => '',
+		'password' => '',
 		'charset'  => 'UTF8'
 	)
 ));
